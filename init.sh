46e23c4772aad531680e7bb04a50861b5cf59612
iboxtips <101639607+iboxtips@users.noreply.github.com>
2022-03-15 18:57:18 +0700

    Edit raddb

--- init.sh
+++ init.sh
@@ -12,27 +12,27 @@ echo "$@"
 [ -z "$GOOGLE_LDAP_USERNAME" ] && echo "GOOGLE_LDAP_USERNAME env variable not defined! Exiting..." && exit 1
 
 # replace all those env params in the file
-sed -i "s|ACCESS_ALLOWED_CIDR|$ACCESS_ALLOWED_CIDR|g" /etc/freeradius/clients.conf
-sed -i "s|SHARED_SECRET|$SHARED_SECRET|g" /etc/freeradius/clients.conf
+sed -i "s|ACCESS_ALLOWED_CIDR|$ACCESS_ALLOWED_CIDR|g" /etc/raddb/clients.conf
+sed -i "s|SHARED_SECRET|$SHARED_SECRET|g" /etc/raddb/clients.conf
 
-sed -i "s|BASE_DOMAIN|$BASE_DOMAIN|g" /etc/freeradius/proxy.conf
-sed -i "s|DOMAIN_EXTENSION|$DOMAIN_EXTENSION|g" /etc/freeradius/proxy.conf
+sed -i "s|BASE_DOMAIN|$BASE_DOMAIN|g" /etc/raddb/proxy.conf
+sed -i "s|DOMAIN_EXTENSION|$DOMAIN_EXTENSION|g" /etc/raddb/proxy.conf
 
-sed -i "s|GOOGLE_LDAP_PASSWORD|$GOOGLE_LDAP_PASSWORD|g" /etc/freeradius/mods-available/ldap
-sed -i "s|GOOGLE_LDAP_USERNAME|$GOOGLE_LDAP_USERNAME|g" /etc/freeradius/mods-available/ldap
+sed -i "s|GOOGLE_LDAP_PASSWORD|$GOOGLE_LDAP_PASSWORD|g" /etc/raddb/mods-available/ldap
+sed -i "s|GOOGLE_LDAP_USERNAME|$GOOGLE_LDAP_USERNAME|g" /etc/raddb/mods-available/ldap
 
 # add support to second level like: .com.br, .com.ar
-sed -i "s|BASE_DOMAIN|$BASE_DOMAIN|g" /etc/freeradius/mods-available/ldap
+sed -i "s|BASE_DOMAIN|$BASE_DOMAIN|g" /etc/raddb/mods-available/ldap
 if [[ ${DOMAIN_EXTENSION} =~ [.] ]]; then
     DOMAIN_EXTENSION=$( echo $DOMAIN_EXTENSION | awk -F'.' '{print $1",dc="$2}' )
 fi
-sed -i "s|DOMAIN_EXTENSION|$DOMAIN_EXTENSION|g" /etc/freeradius/mods-available/ldap
+sed -i "s|DOMAIN_EXTENSION|$DOMAIN_EXTENSION|g" /etc/raddb/mods-available/ldap
 
 # Handle the certs
-cp /certs/ldap-client.key /etc/freeradius/certs/ldap-client.key
-cp /certs/ldap-client.crt /etc/freeradius/certs/ldap-client.crt
-chown freerad:freerad /etc/freeradius/certs/ldap-client*
-chmod 640 /etc/freeradius/certs/ldap-client*
+cp /certs/ldap-client.key /etc/raddb/certs/ldap-client.key
+cp /certs/ldap-client.crt /etc/raddb/certs/ldap-client.crt
+chown freerad:freerad /etc/raddb/certs/ldap-client*
+chmod 640 /etc/raddb/certs/ldap-client*
 
 # Handle the rest of the certificates
 # First the array of files which need 640 permissions
